f3b85eec0a922809c67c34288214302ac67c2cba
kaedynb <kb16qf@brocku.ca>
2020-12-02 20:52:00 -0500

    assignment 4

---
+++ assignments/assignment4.js
@@ -0,0 +1,20 @@
+	var name;
+	var email;
+	var subject;
+	var message;
+function about(){
+	window.location.assign('assignment4.2.html')
+}
+function contact(){
+	window.location.assign('assignment4.3.html')
+}
+function home(){
+	window.location.assign('assignment4.1.html')
+}
+function submitting(){
+	/*document.getElementById("name").submit();
+	document.getElementById("email").submit();
+	document.getElementById("subject").submit();
+	document.getElementById("message").submit();*/
+	alert ('Thank you!');
+}
